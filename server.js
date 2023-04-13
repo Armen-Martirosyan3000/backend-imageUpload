c28ba4d04d6da85a063b165962fe0e4cbf56075c
Armen-Martirosyan3000 <Armen11a1@mail.ru>
2023-04-13 16:51:07 +0400

    change

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//when we have a website and a user on the website wants to upload a picture-երբ ունենք կայք և կայքում օգտագործողը ցանկանում է նկար ներբեռնել, նեևբեռնած նկարը մուտք է լինում images պապկայի մեջ
+// when we have a website and a user on the website wants to upload a picture-երբ ունենք կայք և կայքում օգտագործողը ցանկանում է նկար ներբեռնել, նեևբեռնած նկարը մուտք է լինում images պապկայի մեջ
 const express = require('express');
 const multer = require('multer');//Multer-ը node.js-ի middleware ֆունկցիա է,որը հիմնականում օգտագործվում է ֆայլեր վերբեռնելու համար: 
 const fs = require('fs');//Node FS-ը նշանակում է NodeJS File System մոդուլ,Node FS Module-ն ապահովում է API՝ Ֆայլային համակարգի հետ փոխազդելու և որոշ IO գործողություններ կատարելու համար, ինչպիսիք են ֆայլի ստեղծումը, Ֆայլի ընթերցումը, ֆայլի ջնջումը, ֆայլի թարմացումը և այլն:
@@ -9,11 +9,11 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 const storage2 = multer.diskStorage({
-  destination: function (req, file, cb) {//destination(նպատակակետը)-ը './images' պապկան է
+  destination: function (req, file, cb) { //destination(նպատակակետը)-ը './images' պապկան է
     cb(null, './images')
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)//այստեղով պահպանվում է ֆայլի օրիգինալ անվանումը
+    cb(null, file.originalname) //այստեղով պահպանվում է ֆայլի օրիգինալ անվանումը
   }
 })
 
